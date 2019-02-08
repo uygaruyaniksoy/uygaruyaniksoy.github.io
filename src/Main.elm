571e8b5c1c1daf0c4aa960aa211f705385b72379
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2019-02-08 23:30:30 +0300

    education and experience

--- src/Main.elm
+++ src/Main.elm
@@ -13,20 +13,39 @@ type alias Flags =
 type alias Model =
     { educationItems : List EducationItem
     , selectedEducationItem : Maybe EducationItem
+    , experienceItems : List ExperienceItem
+    , selectedExperienceItem : Maybe ExperienceItem
     }
 
 
 type Msg
     = SelectedEducationItem EducationItem
+    | SelectedExperienceItem ExperienceItem
     | Noop
+
+
+type alias Time =
+    { from : String, to : String }
 
 
 type alias EducationItem =
     { school : String
     , degree : String
-    , time : String
+    , time : Time
     , place : String
     , achievements : List String
+    , img : String
+    }
+
+
+type alias ExperienceItem =
+    { company : String
+    , role : String
+    , time : Time
+    , place : String
+    , achievements : List String
+    , technologies : List String
+    , img : String
     }
 
 
@@ -38,8 +57,80 @@ initialState : Model
 initialState =
     { selectedEducationItem = Nothing
     , educationItems =
-        [ EducationItem "Middle East Technical University" "Computer Engineering" "2013-2019" "Turkey" [ "did sth", "didthe other thung", "and some other thung", "also that", "and this" ]
-        , EducationItem "Regensburg University of Applied Science" "Computer Science" "2017-2017" "Germany" [ "did sth" ]
+        [ EducationItem
+            "Middle East Technical University"
+            "Computer Engineering"
+            (Time
+                "2013"
+                "2019"
+            )
+            "Ankara, Turkey"
+            [ "CENG213 - Data Structures"
+            , "CENG242 - Programming Language Concepts"
+            , "CENG315 - Algorithms"
+            , "CENG334 - Operating Systems"
+            , "CENG435 - Data Communications and Networking"
+            , "CENG451 - Information Systems Development"
+            , "CENG453 - Software Construction"
+            , "CENG466 - Fundamentals of Image Processing"
+            , "CENG477 - Computer Graphics"
+            , "CENG495 - Cloud Computing"
+            ]
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_ODT%C3%9C.svg/480px-Logo_ODT%C3%9C.svg.png"
+        , EducationItem "Regensburg University of Applied Science"
+            "Computer Science"
+            (Time
+                "2017"
+                "2017"
+            )
+            "Regensburg, Germany"
+            [ "Advanced Java Programming"
+            , "App Development for Andriod Smartphones"
+            , "Service Innovation and Modelling"
+            , "Introduction to Web Security"
+            ]
+            "https://www.sensorik-bayern.de/fileadmin/_processed_/8/6/csm_OTH-Regensburg_logo_74970bf8eb.jpg"
+        ]
+    , selectedExperienceItem = Nothing
+    , experienceItems =
+        [ ExperienceItem "Siemens"
+            "Junior Software Developer"
+            (Time "Nov 2018" "Present")
+            "Ankara, Turkey"
+            [ "Implemented a hierarchical diagram for displaying various types of components using GoJS and Angular"
+            ]
+            [ "Angular", "GoJS", "Webpack", "TypeScript" ]
+            "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/032011/siemens.png?itok=rdKsgifk"
+        , ExperienceItem "University College Dublin"
+            "R&D Intern"
+            (Time "Jul 2018" "Sep 2018")
+            "Dublin, Ireland"
+            [ "Worked for a EU funded software project which aims to help children with ADHD to enhance\ntheir reading skills."
+            , "Implemented a hand gesture recognizing system that works with several marker layouts for AR.js."
+            ]
+            [ "A-Frame", "ARJS", "Unity", "Blender", "Vuforia" ]
+            "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/UCD_Dublin.png/200px-UCD_Dublin.png"
+        , ExperienceItem "Halici Yazilim"
+            "Junior Software Developer"
+            (Time "Oct 2017" "Jun 2018")
+            "Ankara, Turkey"
+            [ "Developed a web application for Brainquire users where they can see statistics on themselves or others."
+            , "Implemented features like secure authentication, purchasing interface to buy premium membership."
+            , "Created a library for mobile devices to visualise players performace with graphics and charts."
+            ]
+            [ "React", "React Native", "Redux", "Ruby on Rails", "ExpressJS", "D3JS", "PostgreSQL" ]
+            "https://media.licdn.com/dms/image/C4E0BAQENcpJth4ed1A/company-logo_200_200/0?e=2159024400&v=beta&t=VGDF4ozO3KjgnSdTMI5T8PqfORiDXrnH0TS4ya1YYq0"
+        , ExperienceItem "Kale Yazilim"
+            "Junior Software Developer"
+            (Time "Jun 2016" "Mar 2017")
+            "Ankara, Turkey"
+            [ "Implemented a multifunctional interface that will show the user data from a Neo4J database in a way that is easier to understand from a human’s perspective."
+            , "Worked on an engine that will convert RDBMS data into a Graph DB."
+            , "Developed a single page web application that uses react and local storage."
+            , "Made front-end improvements and bugfixes in the Graph project."
+            ]
+            [ "React", "Redux", "ExpressJS", "Neo4J", "D3JS" ]
+            "https://pbs.twimg.com/profile_images/1082916350639722498/_YHKu7st_400x400.jpg"
         ]
     }
 
@@ -55,7 +146,20 @@ update msg model =
             ( model, Cmd.none )
 
         SelectedEducationItem item ->
-            ( { model | selectedEducationItem = Just item }, Cmd.none )
+            ( { model
+                | selectedEducationItem = Just item
+                , selectedExperienceItem = Nothing
+              }
+            , Cmd.none
+            )
+
+        SelectedExperienceItem item ->
+            ( { model
+                | selectedExperienceItem = Just item
+                , selectedEducationItem = Nothing
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
@@ -67,7 +171,7 @@ view model =
         [ siteHeader
         , bio
         , education model
-        , experience
+        , experience model
         , projects
         , skills
         , siteFooter
@@ -139,23 +243,56 @@ educationItem selectedItem item =
     div
         [ class "notice", class "col-sm-12", onClick (SelectedEducationItem item) ]
         [ div []
-            [ h4 [ style "flex" "1", style "color" "#833" ] [ text item.time ]
-            , h4 [ style "flex" "4", style "color" "#444" ] [ text item.school ]
-            , h4 [ style "flex" "2", style "color" "#444" ] [ text item.degree ]
-            , h4 [ style "flex" "1", style "color" "#833" ] [ text item.place ]
+            [ div [ class "notice notice-float" ]
+                [ p [ style "border-bottom" "#ccc solid 1px" ] [ text item.time.to ]
+                , p [] [ text item.time.from ]
+                ]
+            , img [ src item.img ] []
+            , h4 [] [ text item.school ]
+            , h5 [] [ text item.degree ]
+            , h5 [] [ text item.place ]
             ]
-        , div visibility (List.map (\a -> p [] [ text a ]) item.achievements)
+        , div visibility [ hr [] [] ]
+        , div visibility (List.map (\a -> li [ style "color" "#666" ] [ text a ]) item.achievements)
         ]
 
 
-experience =
+experience model =
     div
         [ class "section-header"
         , class "col-md-6"
         , class "col-sm-12"
         ]
         [ h1 [] [ text "experience" ]
-        , br [] []
+        , div [] (List.map (experienceItem model.selectedExperienceItem) model.experienceItems)
+        ]
+
+
+experienceItem selectedItem item =
+    let
+        visibility =
+            case selectedItem == Just item of
+                True ->
+                    [ style "max-height" "1000px", style "opacity" "1" ]
+
+                False ->
+                    [ style "max-height" "0px", style "opacity" "0" ]
+    in
+    div
+        [ class "notice", class "col-sm-12", onClick (SelectedExperienceItem item) ]
+        [ div []
+            [ div [ class "notice notice-float" ]
+                [ p [ style "border-bottom" "#ccc solid 1px" ] [ text item.time.to ]
+                , p [] [ text item.time.from ]
+                ]
+            , img [ src item.img ] []
+            , h4 [] [ text item.company ]
+            , h5 [] [ text item.role ]
+            , h5 [] [ text item.place ]
+            , h5 [] [ text ("Technologies: " ++ String.join ", " item.technologies) ]
+            ]
+        , div visibility [ hr [] [] ]
+        , div visibility (List.map (\a -> li [ style "color" "#666" ] [ text a ]) item.achievements)
         ]
 
 
@@ -177,7 +314,7 @@ skills =
 
 siteFooter =
     footer
-        []
+        [ class "col-md-12" ]
         [ Html.p [] [ Html.text "footer" ]
         ]
 
